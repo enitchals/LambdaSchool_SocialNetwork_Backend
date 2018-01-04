6d89083c78d8f2f63aa197464f4096936e944a6d
Justin Borek <justinborek@gmail.com>
2018-01-04 17:22:29 -0600

    fixed routes.js by importing postcontrollers

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,11 +1,12 @@
 const userControllerMethods = require ('../controllers/userControllers');
+const postControllerMethods = require ('../controllers/postControllers');
 
 module.exports = (app) => {
   app.route('/new-user').post(userControllerMethods.createUser);
   app.route('/login').post(userControllerMethods.login);
-  app.route('/posts').post(userControllerMethods.createPost);
-  app.route('/posts/:id').get(userControllerMethods.userPosts);
+  app.route('/posts').post(postControllerMethods.createPost);
+  app.route('/posts/:id').get(postControllerMethods.userPosts);
   //in the line above, "id" refers to a user ID in mongoDB
-  app.route('/posts/:id').post(userControllerMethods.addComment);
+  app.route('/posts/:id').post(postControllerMethods.addComment);
   //in the line above, "id" refers to a comment ID in mongoDB
 };
